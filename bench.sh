5bf9ef77d33c678d6a7079833fc152d01962a66b
David Sherret <dsherret@gmail.com>
2026-02-25 16:43:02 -0500

    update

---
+++ bench.sh
@@ -0,0 +1,7 @@
+hyperfine
+  --warmup=3
+  --runs=10
+  "./node_modules/.bin/biome format --write --config-path biome.json"
+  "./node_modules/.bin/oxfmt --config oxcfmt.json"
+  "dprint fmt --config dprint-biome.json"
+  "dprint fmt --config dprint-oxc.json"
