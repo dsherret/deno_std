55cb9a9a193b7f37cea451ee977c58ee9c792949
David Sherret <dsherret@gmail.com>
2026-02-25 16:49:48 -0500

    add node_modules bench

--- bench.sh
+++ bench.sh
@@ -1,8 +1,9 @@
 hyperfine \
-  --warmup=3 \
+  --warmup=5 \
   --runs=10 \
   "./node_modules/.bin/biome format --write --config-path biome.json" \
   "./node_modules/.bin/oxfmt --config oxcfmt.json" \
+  "./node_modules/.bin/dprint fmt --config dprint-oxc.json" \
   "dprint fmt --config dprint-biome.json" \
   "dprint fmt --config dprint-oxc.json" \
   "deno fmt"
